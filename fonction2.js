d5ae92094a304be79be0450d8bfefe367ca4e79b
pinkdoab <divers@doab.fr>
2020-08-21 09:36:34 +0200

    amélioration page produit

--- fonction2.js
+++ fonction2.js
@@ -1,19 +1,15 @@
-var $_GET = [];
-var parts = window.location.search.substr(1).split("&");
-for (var i = 0; i < parts.length; i++) {
-    var temp = parts[i].split("=");
+let $_GET = [];
+let parts = window.location.search.substr(1).split("&");
+console.log(parts)
+for (let i = 0; i < parts.length; i++) {
+    let temp = parts[i].split("=");
     $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
 }
-//console.log($_GET);
-
-
 
 let XHR = new XMLHttpRequest()
-//let url = "http://localhost:3000/api/cameras/5be1ed3f1c9d44000030b061"
 let url = "http://localhost:3000/api/cameras/" + $_GET['id']
-//console.log(url);
-
-
+XHR.open("GET", url, true);
+XHR.send();
 
 XHR.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -23,10 +19,6 @@ XHR.onreadystatechange = function() {
         DessineCarteCamera(section, camera)
     }
 }
-
-XHR.open("GET", url, true);
-XHR.send();
-
 
 function DessineCarteCamera(section, camera) {
 
--- fonction_catalogue.js
+++ fonction_catalogue.js
@@ -1,65 +1,51 @@
 let XHR = new XMLHttpRequest()
 let url = "http://localhost:3000/api/cameras"
+XHR.open("GET", url, true);
+XHR.send();
 
 XHR.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let catalogue_camera = JSON.parse(this.responseText)
-        DessineCatalogueCamera(catalogue_camera)
+        ListeCatalogueCamera(catalogue_camera)
     }
 }
 
-XHR.open("GET", url, true);
-XHR.send();
+function ListeCatalogueCamera(catalogue_camera) {
+    for (let camera in catalogue_camera) {
+        CreaCarteCamera(catalogue_camera[camera])        
+    }
+}
 
+function CreaCarteCamera(camera) {
 
-function DessineCatalogueCamera(catalogue_camera) {
-    //console.log(catalogue_camera);
     let section = document.querySelector('section')
-    for (let ref in catalogue_camera) {
-        DessineCarteCamera(section,  catalogue_camera[ref])
-    }
-}
 
-function DessineCarteCamera(section, camera) {
-
-    /*<div class="col-4" style="padding: 10px;">
-        <div class="card">
-            <img class="card-img-top" src="images/logo.png" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Name</h5>
-                <p class="card-text">price</p>
-                <a href="produit.html" class="stretched-link"></a>
-            </div>
-        </div>
-    </div>*/
-
-    let elementDivCol = document.createElement('div')
+    let elementDivCol = document.createElement('div')           //  <div class="col-4" style="padding: 10px;">
     elementDivCol.className = 'col-4'
     elementDivCol.setAttribute("style", "padding: 10px;");
 
-    let elementDivCard = document.createElement('div')
+    let elementDivCard = document.createElement('div')          //      <div class="card">
     elementDivCard.className = 'card'
 
-    let elementImg = document.createElement('img')
+    let elementImg = document.createElement('img')              //          <img class="card-img-top" src="images/logo.png">
     elementImg.className = 'card-img-top'
     elementImg.setAttribute("src", camera['imageUrl']);
 
-    let elementDivCardBody = document.createElement('div')
+    let elementDivCardBody = document.createElement('div')      //          <div class="card-body">
     elementDivCardBody.className = 'card-body'    
 
-    let elementH5 = document.createElement('h5')
+    let elementH5 = document.createElement('h5')                //              <h5 class="card-title">Name</h5>
     elementH5.className = 'card-title'
     let nomH5 = document.createTextNode(camera['name'])
     elementH5.appendChild(nomH5)
 
-    let elementPrice = document.createElement('p')
+    let elementPrice = document.createElement('p')              //              <p class="card-text">price</p>
     elementPrice.className = 'card-text'
     let nomPrice = document.createTextNode(camera['price'] + '€')
     elementPrice.appendChild(nomPrice)
 
-    let elementLien = document.createElement('a')
+    let elementLien = document.createElement('a')               //              <a class="stretched-link" href="produit.html?id=123456789"></a>
     elementLien.className = 'stretched-link'
-    let nomlien = document.createTextNode(camera['price'])
     elementLien.setAttribute('href', "produit.html?id=" + camera['_id'])
 
 
